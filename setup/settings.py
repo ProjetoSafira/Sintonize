5ce529fc59c6863be9d1d0ad8f0b7471b41e3d5e
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2024-10-21 18:24:00 -0300

    adicionando backend do formulario

--- setup/settings.py
+++ setup/settings.py
@@ -105,8 +105,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'site_sintonize/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'site_sintonize/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from site_sintonize.views import index, politicas_privacidade, sondagem, sobre_nos, equipe, trilha
+from site_sintonize.views import index, politicas_privacidade, sondagem, sobre_nos, equipe, trilha,burnout_survey_view, resultado_view
 
 
 urlpatterns = [
@@ -11,5 +11,7 @@ urlpatterns = [
     path('politicas_privacidade', politicas_privacidade, name='politicas_privacidade'), 
     path('sobre_nos/', sobre_nos, name='sobre_nos'),
     path('equipe/', equipe, name='equipe'),
-    path('thilha/', trilha, name='trilha')
+    path('thilha/', trilha, name='trilha'),
+    path('burnout-survey/', burnout_survey_view, name='burnout_survey'),
+    path('resultado/<int:score>/', resultado_view, name='resultado'),
 ]
