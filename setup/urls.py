0c0c0a8c6916d56147e3a417074a19c637ff830e
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2025-01-13 18:12:55 -0300

    adicionando barra de pesquisa

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from site_sintonize.views import index, politicas_privacidade, sondagem, sobre_nos, equipe, trilha, diagnostico, tratamento, burnout_survey_view, resultado_view
+from site_sintonize.views import index, politicas_privacidade, sondagem, sobre_nos, equipe, trilha, diagnostico, tratamento, burnout_survey_view, resultado_view,search_options
 
 
 urlpatterns = [
@@ -16,4 +16,5 @@ urlpatterns = [
      path('tratamento/', tratamento, name='tratamento'),
     path('burnout-survey/', burnout_survey_view, name='burnout_survey'),
     path('resultado/<int:score>/', resultado_view, name='resultado'),
+    path('search-options/', search_options, name='search_options'),
 ]
