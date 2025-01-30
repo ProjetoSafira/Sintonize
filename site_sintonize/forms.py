6bf37b429c6f487fc83c8ac51128a292b90ce1f2
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2025-01-30 13:58:41 -0300

    adicionando perguntas

--- site_sintonize/forms.py
+++ site_sintonize/forms.py
@@ -7,18 +7,14 @@ class BurnoutSurveyForm(forms.ModelForm):
         model = BurnoutSurvey
         fields = '__all__'
         widgets = {
-            field.name: forms.RadioSelect() 
+            field.name: forms.RadioSelect(attrs={'class': 'custom-radio'}) 
             for field in BurnoutSurvey._meta.get_fields() 
             if isinstance(field, models.IntegerField)
         }
 
-
-
     def __init__(self, *args, **kwargs):
         super(BurnoutSurveyForm, self).__init__(*args, **kwargs)
         for field in self.fields.values():
-            # Garantir que não haja opção vazia
             field.empty_label = None
-            # Reatribuir as opções com os rótulos correspondentes
             field.choices = BurnoutSurvey.STATEMENT_CHOICES
             field.required = True
