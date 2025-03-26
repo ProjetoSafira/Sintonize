bf35dfe93c5f1df7ed3ca0dd07d354af3311773b
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2025-03-26 11:20:42 -0300

    Ajustes no formulario

--- site_sintonize/forms.py
+++ site_sintonize/forms.py
@@ -7,14 +7,26 @@ class BurnoutSurveyForm(forms.ModelForm):
         model = BurnoutSurvey
         fields = '__all__'
         widgets = {
-            field.name: forms.RadioSelect(attrs={'class': 'custom-radio'}) 
-            for field in BurnoutSurvey._meta.get_fields() 
-            if isinstance(field, models.IntegerField)
+            'statement_1': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_2': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_3': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_4': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_5': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_6': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_7': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_8': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_9': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_10': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_11': forms.RadioSelect(attrs={'class': 'custom-radio'}),
+            'statement_12': forms.RadioSelect(attrs={'class': 'custom-radio'})
         }
 
     def __init__(self, *args, **kwargs):
-        super(BurnoutSurveyForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         for field in self.fields.values():
-            field.empty_label = None
-            field.choices = BurnoutSurvey.STATEMENT_CHOICES
             field.required = True
+            # Remove a opção vazia/traços do início
+            if hasattr(field, 'choices'):
+                choices = list(field.choices)
+                if choices and choices[0][1] == '---------':
+                    field.choices = choices[1:]
